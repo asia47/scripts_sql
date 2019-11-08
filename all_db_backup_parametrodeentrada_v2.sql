250882691aa8e74506aad334089bbc4bc63088ee
canoli628@gmail.com <clientedb@dombd.local>
2019-11-08 20:44:23 +0100

    editamos el procedimiento para entrada backup

--- all_db_backup_parametrodeentrada_v2.sql
+++ all_db_backup_parametrodeentrada_v2.sql
@@ -5,15 +5,12 @@ GO
 DROP PROCEDURE IF EXISTS BACKUP_ALL_DB_PARENTRADA
 GO
 
--- SQL Server 2017
--- CREATE OR ALTER
 CREATE PROC BACKUP_ALL_DB_PARENTRADA
     @path VARCHAR(256)
 AS
     -- Declarando variables
     DECLARE
         @name VARCHAR(50), -- database name
-        -- @path VARCHAR(256), -- path for backup files
         @fileName VARCHAR(256), -- filename for backup
         @fileDate VARCHAR(20), -- used for file name
         @backupCount INT
@@ -23,25 +20,24 @@ AS
         name VARCHAR(200)
     )
 
-    -- Crear la Carpeta Backup
-    -- SET @path = 'C:\Backup\'
-
-    -- Includes the date in the filename
+    -- Establecemos o valor para a variable @fileDate como a data en formato YYYYMMDD
     SET @fileDate = CONVERT(VARCHAR(20), GETDATE(), 112)
 
-    -- Includes the date and time in the filename
-    --SET @fileDate = CONVERT(VARCHAR(20), GETDATE(), 112) + '_' + REPLACE(CONVERT(VARCHAR(20), GETDATE(), 108), ':', '')
+    -- Establecemos o valor para a variable @fileDate como a data en formato YYYYMMDD_hh:mm:ss
+    -- SET @fileDate = CONVERT(VARCHAR(20), GETDATE(), 112) + '_' + REPLACE(CONVERT(VARCHAR(20), GETDATE(), 108), ':', '')
 
     INSERT INTO [dbo].#tempBackup (name)
         SELECT name
-        FROM master.dbo.sysdatabases
-        WHERE name in ( 'Northwind','pubs')
-    -- WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb')
+			FROM master.dbo.sysdatabases
+			-- seleccionamos hacer un backup de las bd nortwind y pubs si estan en el sql-server
+			WHERE name in ( 'Northwind','pubs') 
+			-- seleccionamos hacer un backup de todas las bd del sql-server que no sean las especificadas abajo 
+			-- WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb')
 
     SELECT @backupCount = count(*)
         FROM [dbo].#tempBackup
 
-    -- Utilidad: Solo Comprobación Nº Backups a realizar
+    -- mostramos el nro de backups que va a realizar
     print 'Número de BDs a guardar: ' + @backupCount
 
     IF ((@backupCount IS NOT NULL) AND (@backupCount > 0)) BEGIN
@@ -70,6 +66,7 @@ AS
 GO
 
 -- Ejecutar Procedimiento
+-- la carpeta del path de entrada 'C:\Backup\' tiene que estar creada en el pc
 EXEC BACKUP_ALL_DB_PARENTRADA 'C:\Backup\'
 GO
 
